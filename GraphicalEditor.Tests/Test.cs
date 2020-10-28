bd5aedb907c371082924948558ad5a8198ddc6b7
Luis Enrique Reyes Serrano <lenrique.reyesse@gmail.com>
2020-10-27 19:49:57 -0600

    Add Test Project

---
+++ GraphicalEditor.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using GraphicalEditor;
+
+namespace GraphicalEditor.Tests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			
+		}
+	}
+}
