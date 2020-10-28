c0291832f2859b5e43abe809bf885ccfaaf2dd9c
Luis Enrique Reyes Serrano <lenrique.reyesse@gmail.com>
2020-10-28 16:01:35 -0600

    Feature: Fill Region

--- GraphicalLibrary/GraphicEditor.cs
+++ GraphicalLibrary/GraphicEditor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GraphicalLibrary
 {
@@ -93,15 +94,12 @@ namespace GraphicalLibrary
 
 					break;
 
-
 				case "S":
 					// S
 					
 					DisplayImage();
 
 					break;
-
-
 			}
 
 		}
@@ -167,7 +165,58 @@ namespace GraphicalLibrary
 		// also belongs to this region.
 		public static void FillRegion(int x, int y, char c)
 		{
-			
+			int rowLength = image.GetLength(0);
+			int colLength = image.GetLength(1);
+			string pColor = image[y, x].ToString();
+
+			string regionPixels = string.Join(Environment.NewLine,
+			                            FindNeighbors(image, new Tuple<int, int>(y, x), pColor, c.ToString()));
+			image[y, x] = c.ToString();
+		}
+
+
+		private static IEnumerable<Tuple<int, int>> FindNeighbors(string[,] items,
+													 Tuple<int, int> startAt,
+		                                                       string previousColor, string newColor)
+		{
+			if (startAt.Item1 < 0 || startAt.Item1 >= items.GetLength(0) ||
+				startAt.Item2 < 0 || startAt.Item2 >= items.GetLength(1))
+				yield break; 
+			else if (items[startAt.Item1, startAt.Item2] != previousColor)
+				yield break;
+
+			Queue<Tuple<int, int>> agenda = new Queue<Tuple<int, int>>();
+			HashSet<Tuple<int, int>> visited = new HashSet<Tuple<int, int>>() { startAt };
+
+			agenda.Enqueue(startAt);
+
+			while (agenda.Any())
+			{
+				for (int i = agenda.Count - 1; i >= 0; --i)
+				{
+					var point = agenda.Dequeue();
+
+					yield return point;
+
+					// Neighbors
+					var validNeighbors = new Tuple<int, int>[] {
+				 new Tuple<int, int>(point.Item1 - 1, point.Item2),     // Left
+		         new Tuple<int, int>(point.Item1 + 1, point.Item2),     // Right
+		         new Tuple<int, int>(point.Item1, point.Item2 - 1),     // Top
+		         new Tuple<int, int>(point.Item1, point.Item2 + 1),}    // Bottom
+					.Where(p => p.Item1 >= 0 && p.Item1 < items.GetLength(0)) // Within array
+					.Where(p => p.Item2 >= 0 && p.Item2 < items.GetLength(1)) // Within array
+						.Where(p => items[p.Item1, p.Item2] == previousColor)            // Valid point
+					.Where(p => visited.Add(p));                              // Not visited 
+
+					foreach (var p in validNeighbors)
+					{
+						agenda.Enqueue(p);
+						image[p.Item1, p.Item2] = newColor;
+					}
+				}
+
+		 }
 		}
 
 		// S. Show the contents of the current image.
@@ -185,8 +234,6 @@ namespace GraphicalLibrary
 				Console.Write("\n");
 
 			}
-
-
 		}
 	}
 }
